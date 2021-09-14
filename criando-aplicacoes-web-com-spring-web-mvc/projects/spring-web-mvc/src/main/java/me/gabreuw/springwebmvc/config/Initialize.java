14210a0ef4375649884f48f6c1323b10b228a07b
ImGabreuw <imgabreuw.dev@gmail.com>
2021-09-14 14:12:47 -0300

    Aula: 'Trabalhando com o Status Code da aplicação'

---
+++ criando-aplicacoes-web-com-spring-web-mvc/projects/spring-web-mvc/src/main/java/me/gabreuw/springwebmvc/config/Initialize.java
@@ -0,0 +1,22 @@
+package me.gabreuw.springwebmvc.config;
+
+import lombok.RequiredArgsConstructor;
+import me.gabreuw.springwebmvc.model.Jedi;
+import me.gabreuw.springwebmvc.repository.JediRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class Initialize implements CommandLineRunner {
+
+    private final JediRepository repository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        repository.save(
+                new Jedi(1L, "Luke", "Skywalker")
+        );
+    }
+
+}
