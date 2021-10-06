48ad324fe25f5b499d74254e34004955a6699c72
ImGabreuw <imgabreuw.dev@gmail.com>
2021-10-06 17:11:24 -0300

    Aula: 'Praticando com Spring: Padroes de projetos em uma API REST'

--- explorando-padroes-de-prrojetos-na-pratica-com-java/projects/padroes-de-projeto-spring/src/main/java/me/gabreuw/padroesdeprojetospring/service/ClienteServiceImpl.java
+++ explorando-padroes-de-prrojetos-na-pratica-com-java/projects/padroes-de-projeto-spring/src/main/java/me/gabreuw/padroesdeprojetospring/service/ClienteServiceImpl.java
@@ -1,34 +1,63 @@
 package me.gabreuw.padroesdeprojetospring.service;
 
+import lombok.RequiredArgsConstructor;
 import me.gabreuw.padroesdeprojetospring.model.Cliente;
+import me.gabreuw.padroesdeprojetospring.model.Endereco;
+import me.gabreuw.padroesdeprojetospring.repository.ClienteRepository;
+import me.gabreuw.padroesdeprojetospring.repository.EnderecoRepository;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class ClienteServiceImpl implements ClienteService {
+
+    private final ClienteRepository clienteRepository;
+    private final EnderecoRepository enderecoRepository;
+    private final ViaCepService viaCepService;
 
     @Override
     public Iterable<Cliente> buscarTodos() {
-        // TODO
-        return null;
+        return clienteRepository.findAll();
     }
 
     @Override
     public Cliente buscarPorId(Long id) {
-        // TODO
-        return null;
+        return clienteRepository
+                .findById(id)
+                .get();
     }
 
     @Override
     public void inserir(Cliente cliente) {
-        // TODO
+        salverClienteComCep(cliente);
     }
 
     @Override
     public void atualizar(Cliente cliente) {
-        // TODO
+        clienteRepository
+                .findById(cliente.getId())
+                .ifPresent(this::salverClienteComCep);
     }
 
     @Override
     public void deletar(Long id) {
-        // TODO
+        clienteRepository.deleteById(id);
+    }
+
+    private void salverClienteComCep(Cliente cliente) {
+        String cep = cliente.getEndereco().getCep();
+
+        Endereco endereco = enderecoRepository
+                .findById(cep)
+                .orElseGet(() -> {
+                    Endereco novoEndereco = viaCepService.consultarCep(cep);
+
+                    return enderecoRepository.save(novoEndereco);
+                });
+
+        cliente.setEndereco(endereco);
+
+        clienteRepository.save(cliente);
     }
 
 }
